84fcc937bbd10e59676b500e3dda0de2e5f10ace
Aspopnly <2756283851@qq.com>
2022-12-04 11:03:49 +0800

    commit 1

---
+++ UC/Configration/WTRconfig.h
@@ -0,0 +1,19 @@
+#pragma once
+
+//1.VESC configration
+
+    //#define wtrcfg_VESC_COMMAND_DUTY_MAX 
+    //#define wtrcfg_VESC_COMMAND_CURRENT_MAX
+    //#define wtrcfg_VESC_COMMAND_POS_MAX
+    //#define wtrcfg_VESC_COMMAND_ERPM_MAX
+
+//1.1VESC can
+
+    #define __VESC_CAN_ENABLE 1
+    #define wtrcfg_VESC_MODE_CAN 1
+
+//1.2VESC uart
+
+    //#define wtrcfg_VESC_MODE_UART 0
+    #define wtrcfg_VESC_MODE_UART 
+    //#define wtrcfg_VESC_UART_TIMEOUT
